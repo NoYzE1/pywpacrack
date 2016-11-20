04d9e482e96d6feee5862873ae67adfca87dee30
NoYzE1 <NoYzE@hotmail.de>
2016-11-20 23:12:57 +0100

    Refactoring and Multithreading! swoosh!

--- wpacrack.py
+++ wpacrack.py
@@ -2,27 +2,28 @@ import hashlib
 import hmac
 import sys
 import time
+import multiprocessing
 
 f = open("{0}".format(sys.argv[1]), "r")
 
 passwords = open("{0}".format(sys.argv[2]), "r")
 
-essid = f.readline().strip("\n")
-amacstr = f.readline()
-amac = []
-smacstr = f.readline()
-smac = []
-anoncestr = f.readline()
-anonce = []
-snoncestr = f.readline()
-snonce = []
-datastr = f.readline()
-micstr = f.readline()
-mic = []
-cmik = b''
-
-data = []
-pmk = b''
+class Data:
+    essid = f.readline().strip("\n")
+    amacstr = f.readline()
+    amac = []
+    smacstr = f.readline()
+    smac = []
+    anoncestr = f.readline()
+    anonce = []
+    snoncestr = f.readline()
+    snonce = []
+    datastr = f.readline()
+    micstr = f.readline()
+    mic = []
+    cmik = b''
+    data = []
+    pmk = b''
 
 def str_to_hex(string):
     string = string.strip("\n")
@@ -74,13 +75,22 @@ def calculate_MIC(ptk, data):
     h = hashlib.sha1()
     return hmac.new(ptk[0:16], data, "sha1").digest()[0:16]
 
-amac = str_to_hex(amacstr)
-smac = str_to_hex(smacstr)
-anonce = str_to_hex(anoncestr)
-snonce = str_to_hex(snoncestr)
-mic = str_to_hex(micstr)
-mic = bytes(mic)
+def calculate_all(password):
+    pmk = calculate_PMK(password, Data.essid)
+    ptk = calculate_PTK(Data.amac, Data.smac, Data.anonce, Data.snonce, pmk)
+    cmic = calculate_MIC(ptk, Data.data)
+    return [pmk, ptk, cmic]
 
+Data.amac = str_to_hex(Data.amacstr)
+Data.smac = str_to_hex(Data.smacstr)
+Data.anonce = str_to_hex(Data.anoncestr)
+Data.snonce = str_to_hex(Data.snoncestr)
+Data.mic = str_to_hex(Data.micstr)
+Data.mic = bytes(Data.mic)
+Data.data = process_data(Data.datastr)
+
+cpu_count = multiprocessing.cpu_count()
+p = multiprocessing.Pool(4)
 counter = 0
 kps_counter = 0
 kps = 0
@@ -88,28 +98,35 @@ ts = time.time()
 ts2 = time.time()
 
 while True:
-    password = passwords.readline().strip("\n")
-    if password != "":
-        counter += 1
+    password = []
+    pmk = []
+    ptk = []
+    cmic = []
+    for i in range(cpu_count):
+        password.append(passwords.readline().strip("\n"))
+    if password[0] != "":
+        counter += len(password)
+        results = p.map(calculate_all, password)
+        for i in range(len(results)):
+            pmk.append(results[i][0])
+            ptk.append(results[i][1])
+            cmic.append(results[i][2])
         if time.time() - ts >= 1:
             kps = counter - kps_counter
             kps_counter = counter
             ts = time.time()
-        pmk = calculate_PMK(password, essid)
-        ptk = calculate_PTK(amac, smac, anonce, snonce, pmk)
-        data = process_data(datastr)
-        cmic = calculate_MIC(ptk, data)
         if time.time() - ts2 >= 0.05:
             print("Keys tested: {0} ({1} k/s)".format(counter, kps))
-            print("Current Passphrase: {0}".format(password))
-            print("Master Key: ", bytes_to_hex(pmk))
-            print("Transient Key: ", bytes_to_hex(ptk))
-            print("Message Integrity Check: ", bytes_to_hex(cmic))
+            print("Current Passphrase: {0}".format(password[0]))
+            print("Master Key: ", bytes_to_hex(pmk[0]))
+            print("Transient Key: ", bytes_to_hex(ptk[0]))
+            print("Message Integrity Check: ", bytes_to_hex(cmic[0]))
             print()
             ts2 = time.time()
-        if cmic == mic:
-            print("Key found! [ {0} ]".format(password))
-            break
+        for i in range(len(password)):
+            if cmic[i] == Data.mic:
+                print("Key found! [ {0} ]".format(password[i]))
+                exit(0)
     else:
         print("Passphrase not in Dictionary!")
         break
